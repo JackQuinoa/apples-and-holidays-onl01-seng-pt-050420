b24c33a9912e62cc2c3cd4e70a3ece7af68d9d58
Jack Quinoa <jquinoa208@gmail.com>
2020-05-17 04:28:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supplies|
+      puts "#{event.to_s("_")"
     
     end
   end
